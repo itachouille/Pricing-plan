6c65b0bd2dc46bf5cef48f2a31d79f447e01eae3
Anthony David <anthony.david34@sfr.fr>
2024-12-05 11:25:29 +0100

    card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,41 @@
-const Card = () => {
-  return <div>Card</div>;
+import Button from "./Button";
+
+interface CardProps {
+  icon: string;
+  title: string;
+  price: number;
+  list: string[];
+  text?: string;
+  badge?: string;
+}
+
+const Card = ({ icon, title, price, list, text, badge }: CardProps) => {
+  return (
+    <div className="card">
+      <div className="w-[46px] h-[46px] border border-white flex justify-center">
+        <img src={icon} alt="icon" width={26} height={26} />
+      </div>
+      <div>
+        <span className="font-medium text-[33px]">{title}</span>
+        {badge}
+      </div>
+      <div className="">${price} /month</div>
+      <div className="mx-auto w-2/4 h-0.5 bg-white" />
+      <div>
+        <ul>
+          {list.map((item, index) => (
+            <li className="flex" key={index}>
+              <img src="./check.svg" alt="check icon" />
+              {item}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>{text}</div>
+
+      <Button variant="btn" label="Je m'abonne" />
+    </div>
+  );
 };
 
 export default Card;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -29,6 +29,10 @@ export default {
           "@apply border border-[#FFFFFF]/40 text-[#FFFFFF] text-lg font-bold bg-transparent w-[275px] h-[45px] rounded-lg":
             {},
         },
+        ".card": {
+          "@apply border border-[#E7EBFF] text-[#FFFFFF] rounded-lg p-[30px] bg-gradient-to-t from-[#031953] to-[#1770F7]":
+            {},
+        },
       });
       addUtilities({});
     }),
